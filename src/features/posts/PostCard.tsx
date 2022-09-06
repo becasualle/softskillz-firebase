f84fd2ef7955082a194b5addbba68b002a9891a9
Mark <mark.nelubin@gmail.com>
2022-09-06 15:25:13 +0300

    added images and link to card

--- src/features/posts/PostCard.tsx
+++ src/features/posts/PostCard.tsx
@@ -1,6 +1,8 @@
 import { NextPage } from "next";
+import Link from "next/link";
 import { Post } from "./postsSlice";
 import styles from "./PostCard.module.scss"
+import Image from 'next/image';
 
 interface Props {
     post: Post
@@ -9,8 +11,21 @@ interface Props {
 const PostCard: NextPage<Props> = ({ post }) => {
     return (
         <article className={styles.post}>
+            <div className={styles["post-image"]}>
+                <Link href={`/posts/${encodeURIComponent(post.id)}`} >
+                    <Image
+                        src="https://images.unsplash.com/photo-1662411716147-3f6f6cc32399?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
+                        alt="post image"
+                        width={1152}
+                        height={767}
+                        className="link"
+                    />
+                </Link>
+            </div>
             <div className={styles["post-text"]}>
-                <h3 className="text__title">{post.title}</h3>
+                <Link href={`/posts/${encodeURIComponent(post.id)}`}>
+                    <h3 className="text__title link">{post.title}</h3>
+                </Link>
                 <p className="text__para">{post.body}</p>
             </div>
         </article>
