9237583f467e5654cf33ab5f5ac335f1a663f610
Mark <mark.nelubin@gmail.com>
2022-09-12 18:28:43 +0300

    login page styling, static create note page

--- src/pages/notes/create-note.tsx
+++ src/pages/notes/create-note.tsx
@@ -1,5 +1,6 @@
 import { NextPage } from 'next'
 import React from 'react'
+import styles from './create-note.module.scss'
 
 interface Props {
 
@@ -7,7 +8,20 @@ interface Props {
 
 const createNote: NextPage<Props> = () => {
     return (
-        <div>createNote</div>
+        <div className={styles.createPostPage}>
+            <div className={styles.cpContainer}>
+                <h1>Разберите своё состояние</h1>
+                <div className={styles.inputGp}>
+                    <label htmlFor="title">Заголовок</label>
+                    <input type="text" placeholder='Заголовок...' id='title' name='title' />
+                </div>
+                <div className={styles.inputGp}>
+                    <label htmlFor="post">Ситуация</label>
+                    <textarea placeholder='Ситуация...' id='post' name='post' />
+                </div>
+                <button> Сохранить запись </button>
+            </div>
+        </div>
     )
 }
 
