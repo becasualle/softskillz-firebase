78e8fe2ec95bad7b7506caec8127121a92df2e20
Mark Nelyubin <mark.nelubin@gmail.com>
2022-09-15 11:55:09 +0300

    added prettier

--- src/pages/notes/create-note.tsx
+++ src/pages/notes/create-note.tsx
@@ -1,60 +1,78 @@
-import { NextPage } from 'next'
-import React, { useEffect, useState } from 'react'
-import styles from './create-note.module.scss'
+import { NextPage } from "next";
+import React, { useEffect, useState } from "react";
+import styles from "./create-note.module.scss";
 import { addDoc, collection } from "firebase/firestore";
-import { db, auth } from '../../firebase-config';
-import { useRouter } from 'next/router';
-import { useGlobalContext } from '../../context';
-import { Author, PostNote, Note } from '../../features/posts/notesSlice';
-import dayjs from 'dayjs';
+import { db, auth } from "../../firebase-config";
+import { useRouter } from "next/router";
+import { useGlobalContext } from "../../context";
+import { Author, PostNote, Note } from "../../features/posts/notesSlice";
+import dayjs from "dayjs";
 
-interface Props {
-
-}
+interface Props {}
 
 const createNote: NextPage<Props> = () => {
-    const [noteContent, setNoteContent] = useState({ title: "", text: "" });
+  const [noteContent, setNoteContent] = useState({ title: "", text: "" });
 
-    const router = useRouter();
+  const router = useRouter();
 
-    const notesCollectionRef = collection(db, 'notes');
+  const notesCollectionRef = collection(db, "notes");
 
-    const { isAuth } = useGlobalContext();
+  const { isAuth } = useGlobalContext();
 
-    const createNote = async () => {
-        const createdAt = dayjs().format('YYYY-MM-DD HH:mm:ss');
-        const author: Author = {
-            name: auth.currentUser.displayName, id: auth.currentUser.uid, email: auth.currentUser.email
-        }
-        const note: PostNote = {
-            title: noteContent.title, text: noteContent.text, author, createdAt
-        }
-        await addDoc(notesCollectionRef, note)
-        router.push('/notes');
+  const createNote = async () => {
+    const createdAt = dayjs().format("YYYY-MM-DD HH:mm:ss");
+    const author: Author = {
+      name: auth.currentUser.displayName,
+      id: auth.currentUser.uid,
+      email: auth.currentUser.email,
+    };
+    const note: PostNote = {
+      title: noteContent.title,
+      text: noteContent.text,
+      author,
+      createdAt,
+    };
+    await addDoc(notesCollectionRef, note);
+    router.push("/notes");
+  };
+
+  useEffect(() => {
+    if (!isAuth && !localStorage.getItem("isAuth")) {
+      router.push("/login");
     }
+  }, []);
 
-    useEffect(() => {
-        if (!isAuth && !localStorage.getItem('isAuth')) {
-            router.push('/login')
-        }
-    }, [])
-
-    return (
-        <div className={styles.createPostPage}>
-            <div className={styles.cpContainer}>
-                <h1>Разберите своё состояние</h1>
-                <div className={styles.inputGp}>
-                    <label htmlFor="title">Заголовок</label>
-                    <input type="text" placeholder='Заголовок...' id='title' name='title' onChange={(e) => setNoteContent({ ...noteContent, title: e.target.value })} />
-                </div>
-                <div className={styles.inputGp}>
-                    <label htmlFor="post">Ситуация</label>
-                    <textarea placeholder='Ситуация...' id='post' name='post' onChange={(e) => setNoteContent({ ...noteContent, text: e.target.value })} />
-                </div>
-                <button onClick={createNote}> Сохранить запись </button>
-            </div>
+  return (
+    <div className={styles.createPostPage}>
+      <div className={styles.cpContainer}>
+        <h1>Разберите своё состояние</h1>
+        <div className={styles.inputGp}>
+          <label htmlFor="title">Заголовок</label>
+          <input
+            type="text"
+            placeholder="Заголовок..."
+            id="title"
+            name="title"
+            onChange={(e) =>
+              setNoteContent({ ...noteContent, title: e.target.value })
+            }
+          />
         </div>
-    )
-}
+        <div className={styles.inputGp}>
+          <label htmlFor="post">Ситуация</label>
+          <textarea
+            placeholder="Ситуация..."
+            id="post"
+            name="post"
+            onChange={(e) =>
+              setNoteContent({ ...noteContent, text: e.target.value })
+            }
+          />
+        </div>
+        <button onClick={createNote}> Сохранить запись </button>
+      </div>
+    </div>
+  );
+};
 
-export default createNote
+export default createNote;
--- src/pages/notes/index.tsx
+++ src/pages/notes/index.tsx
@@ -1,47 +1,53 @@
-import { NextPage } from 'next'
-import React, { useEffect, useState } from 'react'
-import { useAppDispatch, useAppSelector } from '../../app/hooks';
+import { NextPage } from "next";
+import React, { useEffect, useState } from "react";
+import { useAppDispatch, useAppSelector } from "../../app/hooks";
 import SubHeader from "../../layout/header/SubHeader";
 import PostsGrid from "../../features/posts/PostsGrid";
-import { useGlobalContext } from '../../context';
-import { useRouter } from 'next/router';
-import { auth } from '../../firebase-config';
-import { fetchAllNotes, selectNotes } from '../../features/posts/notesSlice';
+import { useGlobalContext } from "../../context";
+import { useRouter } from "next/router";
+import { auth } from "../../firebase-config";
+import { fetchAllNotes, selectNotes } from "../../features/posts/notesSlice";
 
-interface Props {
-}
+interface Props {}
 
 const Posts: NextPage<Props> = () => {
-    const [searchText, setSearchText] = useState('');
-    const { isAuth } = useGlobalContext();
-    const router = useRouter();
-    const dispatch = useAppDispatch();
-    const notes = useAppSelector(selectNotes);
+  const [searchText, setSearchText] = useState("");
+  const { isAuth } = useGlobalContext();
+  const router = useRouter();
+  const dispatch = useAppDispatch();
+  const notes = useAppSelector(selectNotes);
 
-    useEffect(() => {
-        if (!isAuth && !localStorage.getItem('isAuth')) {
-            router.push('/login')
-        } else {
-            dispatch(fetchAllNotes());
-        }
-    }, [])
-
-    const handleSearch: React.ChangeEventHandler<HTMLInputElement> = (e) => {
-        setSearchText(e.target.value);
+  useEffect(() => {
+    if (!isAuth && !localStorage.getItem("isAuth")) {
+      router.push("/login");
+    } else {
+      dispatch(fetchAllNotes());
     }
+  }, []);
 
-    const filteredNotes = notes.filter((note) => {
-        return note.author.id === auth.currentUser.uid && note.title.toLowerCase().includes(searchText.toLowerCase());
-    })
+  const handleSearch: React.ChangeEventHandler<HTMLInputElement> = (e) => {
+    setSearchText(e.target.value);
+  };
 
+  const filteredNotes = notes.filter((note) => {
     return (
-        <section className='posts'>
-            <SubHeader title="Прорабатывайте свои установки" />
-            <input type="text" value={searchText} onChange={handleSearch} />
-            {filteredNotes.length ? <PostsGrid posts={filteredNotes} /> : <div>Нет результатов</div>}
-        </section>
-    )
-}
+      note.author.id === auth.currentUser.uid &&
+      note.title.toLowerCase().includes(searchText.toLowerCase())
+    );
+  });
+
+  return (
+    <section className="posts">
+      <SubHeader title="Прорабатывайте свои установки" />
+      <input type="text" value={searchText} onChange={handleSearch} />
+      {filteredNotes.length ? (
+        <PostsGrid posts={filteredNotes} />
+      ) : (
+        <div>Нет результатов</div>
+      )}
+    </section>
+  );
+};
 
 // export const getServerSideProps: GetServerSideProps = async () => {
 //     const notes = await getNotes();
@@ -52,4 +58,4 @@ const Posts: NextPage<Props> = () => {
 //     }
 // }
 
-export default Posts
+export default Posts;
