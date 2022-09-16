8515164f1b461cf1340d42ca69481f850154c8ff
Mark Nelyubin <mark.nelubin@gmail.com>
2022-09-16 12:49:49 +0300

    testimonials styles

--- src/components/Testimonials/index.tsx
+++ src/components/Testimonials/index.tsx
@@ -11,14 +11,53 @@ export interface Testimonial {
   author: TestimonialAuthor;
   emphasized: string;
   body: string;
+  color: string;
 }
 
 type Props = React.PropsWithChildren<{ testimonials: Testimonial[] }>;
 
-const Testimonials = (props: Props) => {
+const Testimonials = ({ testimonials }: Props) => {
+  const getTestimonialClasses = (color: string) => {
+    return [
+      styles["testimonial"],
+      color === "green" && styles["testimonial--bg-green"],
+      color === "yellow" && styles["testimonial--bg-yellow"],
+      color === "purple" && styles["testimonial--bg-purple"],
+      color === "blue" && styles["testimonial--bg-blue"],
+    ]
+      .filter((e) => e)
+      .join(" ");
+  };
+
   return (
-    <section className={styles.testimonials}>
-      <h1>What's Different About Manage?</h1>
+    <section className={styles["testimonial-page"]}>
+      <h2 className={styles["testimonial-page__title"]}>
+        What's Different About Manage?
+      </h2>
+      <div className={styles.testimonials}>
+        {testimonials.map((testimonial) => {
+          const testimonialClasses = getTestimonialClasses(testimonial.color);
+          const { author, emphasized, body } = testimonial;
+
+          return (
+            <div className={testimonialClasses} key={testimonial.body}>
+              <header className={styles.testimonial__header}>
+                <img
+                  src={author.avatar}
+                  className={styles.testimonial__img}
+                  alt="author avatar"
+                />
+                <div>
+                  <h3>{author.name}</h3>
+                  <p>{author.position}</p>
+                </div>
+              </header>
+              <p className={styles.testimonial__lead}>{emphasized}</p>
+              <p className={styles.testimonial__quote}>{body}</p>
+            </div>
+          );
+        })}
+      </div>
     </section>
   );
 };
